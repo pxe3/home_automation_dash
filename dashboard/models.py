f1f9cc43ff14017c00052fdc7de5d7a7023e83d2
Arya Prakash <aryaprak@uw.edu>
2023-08-08 14:57:14 -0800

    8 8 23
    managed to  get 127 0 0 1 8000 working mostly

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class SmartLight(models.Model):
+    name = models.CharField(max_length=100)
+    is_on = models.BooleanField(default=False)
+    brightness = models.PositiveIntegerField(default=0)
+    room = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+
+class SmartFan(models.Model):
+    name = models.CharField(max_length=100)
+    is_on = models.BooleanField(default=False)
+    speed = models.PositiveIntegerField(default=0)
+    rotating = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import SmartLight
+from .models import SmartFan
 
 # Create your views here.
+
+def smart_lights(request):
+    lights = SmartLight.objects.all()
+    return render(request, 'dashboard/smart_lights.html', {'lights': lights})
+
+
+def smart_fans(request):
+    fans = SmartFan.objects.all()
+    return render(request, 'dashboard/smart_fans.html', {'fans': fans})
